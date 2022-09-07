f8a53a46ba7b835e43d9e14316524058e36a4b21
Agustin Salum <agustinsalum92@hotmail.com>
2022-09-06 23:47:32 -0300

    Realizado ejercicio 15 y 16

--- practica1/p1e15y16.rb
+++ practica1/p1e15y16.rb
@@ -6,6 +6,12 @@
 def rot13(string)
     string.tr("A-Za-z", "N-ZA-Mn-za-m")
 end
+
+def rot13_con_cantidad(cadena,cantidad)
+    nueva_cadena=cadena.split("")
+    (nueva_cadena.map { |c| (c.ord + cantidad).chr if c.match?(/[a-zA-Z0-9&_\.-]/)}).join('')
+end
   
 p rot13("¡abcd!")
+p rot13_con_cantidad("¡abcd!",13)
 
