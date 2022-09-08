b540be58022ec39ee76f018acc356365dbfe6d94
Agustin Salum <agustinsalum92@hotmail.com>
2022-09-07 22:14:13 -0300

    Realizando ejercicio 22

---
+++ practica1/p1e21.rb
@@ -0,0 +1,7 @@
+
+def sumar_tres_cinco(tope)
+    (0..tope).to_a.select { |x| x % 3 == 0 || x % 5 == 0 }.inject(:+)
+end
+
+
+p sumar_tres_cinco(100)
---
+++ practica1/p1e22.rb
@@ -0,0 +1,7 @@
+
+def sumar_multiplos(arreglo,tope)
+    (0..tope).to_a.select {|x| x % 3 == 0 || x % 5 == 0 }.inject(:+)
+end
+
+
+p sumar_tres_cinco(100)
