d5295cd5d479c09054316cf06124ee135fffbb6a
Agustin Salum <agustinsalum92@hotmail.com>
2022-09-09 01:44:26 -0300

    Ultimo ejercicio resuelto

--- practica1/p1e22.rb
+++ practica1/p1e22.rb
@@ -1,25 +1,23 @@
 
-def sumar_multiplos(arreglo,tope)
-    total = 0
-    for i in arreglo do
-        total+= ((0..tope).to_a.select { |x| x % i  == 0}.inject(:+))
-    end
-    total
+
+def multiplos_de(arreglo,tope)
+    # Seleccionamos los numeros del rango que son multiplos de todos los numeros del arreglo
+    # Select recorre los numeros del rango
+    # Map recorre todos los numeros del arreglo
+    # numero_rango % num == 0 --> Si numero de rango es multiplo de num retorna true para esa posicion
+    # all? retorna True si todas las posiciones dieron True
+    # Si todas las posiciones dieron true el select guarda ese numero de rango que fue multiplo de todos
+    # Por ultimo, una vez obtenidos todos los numeros que cumplen, hacemos la sumatoria con inject 
+    ((0..tope).select { |numero_rango| (arreglo.map { |num| numero_rango % num == 0}).all?}).inject(:+)
 end
 
+p multiplos_de([3, 5], 100)
+p multiplos_de([3, 5, 17, 28, 65], 100_000)
 
-p sumar_multiplos([3, 5, 17, 28, 65], 100_000)
-p sumar_multiplos([3, 5, 17], 10)
-
+=begin
+Ayuda:
 irb(main):023:0> [3, 5, 17, 28, 65].map {|i| 5 % i}
 => [2, 0, 5, 5, 5]
-irb(main):024:0> [3, 5, 17, 28, 65].map {|i| 5 % i}
-=> true
 irb(main):022:0> [true, false].all?
 => false
-irb(main):023:0> [3, 5, 17, 28, 65].map {|i| 5 % i}
-=> [2, 0, 5, 5, 5]
-irb(main):024:0> [3, 5, 17, 28, 65].map {|i| 5 % i}
-=> [2, 0, 5, 5, 5]
-irb(main):025:0> [true, false].all?
-                                         
+=end
