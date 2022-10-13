f31815a9eb762cd69d41ba14f800fc98cfbab963
Agustin Salum <agustinsalum92@hotmail.com>
2022-10-13 00:23:17 -0300

    Agregando comentarios

--- practica2/enumeradores/p2e21-con_lazy.rb
+++ practica2/enumeradores/p2e21-con_lazy.rb
@@ -1,12 +1,12 @@
 
 # Lazy soporta rangos infinitos porque no carga todo en memoria, a diferencia del enumerator comun
 # enumerator a partir de una colección.
-def primos_lazy_mio(tope)
+def primos_lazy(tope)
         ((2..Float::INFINITY).lazy.reject { |numero| (2...numero).any? { |num| numero % num == 0 } }).take(tope)
 end
 
 begin # La excepcion se captura afuera y no adentro de la funcion porque se lanza en el momento que se hace next
-    enumerador_lazy_m = primos_lazy_mio(4)
+    enumerador_lazy_m = primos_lazy(4)
     p enumerador_lazy_m.next
     p enumerador_lazy_m.next
     p enumerador_lazy_m.next
