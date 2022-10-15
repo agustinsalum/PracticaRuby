33ce4484e50450bd9e9ebde1e3e51ceca80aadd6
Agustin Salum <agustinsalum92@hotmail.com>
2022-10-15 18:43:12 -0300

    Realizado todos los ejercicios de excepciones

--- practica3/excepciones/p3e10/p3e10.rb
+++ practica3/excepciones/p3e10/p3e10.rb
@@ -1,25 +1,35 @@
 
-def verificar(palabra)
-    p palabra.scan(/[a-z]/i)
+def sosUnNumero?(palabra)
+    ((palabra.scan(/[a-z]/i)).empty?) ? (return palabra.to_i) : (return palabra)  # Expresiones regulares
 end
 
 def procesar_archivo(file)
     archivo = File.open(file)
     coleccion_palabras = []
-    while linea = archivo.gets # obtenemos toda la linea con el salto de linea /n
-        palabra = linea.chomp # eliminamos el salto de linea /n
-        verificar(palabra)
-        coleccion_palabras << palabra
+    while linea = archivo.gets  # obtenemos toda la linea con el salto de linea /n
+        dato = linea.chomp   # eliminamos el salto de linea /n
+        dato = sosUnNumero?(dato)       # Solucion a que todo nos retorna como string
+        coleccion_palabras << dato
     end
     p coleccion_palabras
     contador = 0
     begin
-        coleccion_palabras.map do |palabra|
-
-
+        p "empieza"
+        coleccion_palabras.map do |dato|
+            yield(dato)
+            puts dato
+            contador+=1
+            coleccion_palabras.delete(dato)
+            p coleccion_palabras
         end
-    rescue
-        "nada"
+    rescue ArgumentError => mensaje
+        puts "Error encontrado: #{mensaje}"
+        gets()
+        coleccion_palabras.delete(dato)
+        p dato
+        p coleccion_palabras
+        retry
+        
     end
 end
 
